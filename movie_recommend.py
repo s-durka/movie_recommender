fd263bff4ca69ac6b57b4b4d38ab594174508ffc
Stanisław Durka <stas.durka@gmail.com>
2025-02-11 18:44:04 +0100

    add printing movie names instead of IDs

--- movie_recommend.py
+++ movie_recommend.py
@@ -50,8 +50,19 @@ def recommend_films(user_id, n, knn, ratings):
 
     return total_scores[:n]['uid']
 
+def load_movie_names(file_path):
+    # Load the u.item file
+    df = pd.read_csv(file_path, sep='|', header=None, encoding='latin-1',
+                     names=['movie_id', 'movie_title', 'release_date', 'video_release_date', 'IMDb_URL'] + [f'genre_{i}' for i in range(19)])
+    
+    # Create a dictionary mapping movie_id to movie_title
+    movie_dict = pd.Series(df.movie_title.values, index=df.movie_id).to_dict()
+    return movie_dict
+
 def main():
     df = load_data('./ml-100k/u.data')
+    movie_dict = load_movie_names('./ml-100k/u.item')
+
     matrix_sparse = create_matrix(df, USERS, FILMS)
 
     neigh = NearestNeighbors(n_neighbors=NEIGHBORS + 1, metric='cosine')
@@ -61,7 +72,8 @@ def main():
     TOP_N = 5
 
     result = recommend_films(USER_ID, TOP_N, neigh, matrix_sparse)
-    print(f"Hi, User {USER_ID}. Based on your other reviews, here are {TOP_N} films you might like: {', '.join(map(str, result))}")
+    result = [movie_dict[i] for i in result]
+    print(f"Hi, User {USER_ID}. Based on your other reviews, here are {TOP_N} films you might like:\n{ ',\n'.join(map(str, result)) }")
 
 if __name__ == "__main__":
     main()
